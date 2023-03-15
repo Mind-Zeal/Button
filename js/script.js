0b9ff47a1ede1e59252b03498dd85763ba4b8389
Mykola Zahorulko <122747999+Mind-Zeal@users.noreply.github.com>
2023-03-15 18:12:21 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,61 @@
+$( document ).ready(function() {
+	var arrow = $(".arrow"),
+		button = $(".wave-btn");
+		pos = $('.wave__container').height() * 2;
+	arrow.click(function() {
+		window.scrollTo({
+			top: pos,
+			left: 0,
+			behavior: "smooth"	
+		});
+		if (arrow.hasClass("_active")) {
+			window.scrollTo({
+				top: 0,
+				left: 0,
+				behavior: "smooth"	
+			});
+		}
+	});
+	button.click(function() {
+		window.scrollTo({
+			top: pos,
+			left: 0,
+			behavior: "smooth"	
+		});
+	});
+	function count() {
+		$(window).scroll(function(event) {
+			var
+			cPos = $('.wave__container').height(),
+			topWindow = window.pageYOffset;
+			if (topWindow > (cPos + (cPos / 2))) {
+				arrow.addClass( "_active" );
+			} else {
+				arrow.removeClass( "_active" );
+			}
+		});
+	};
+	count();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
